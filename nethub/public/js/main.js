84c54b0efb7bbe7fd34d6ccd51e96b39277d153a
Oliver <olieri@kth.se>
2018-11-01 00:18:14 +0100

    initial commit (with alot of content)

---
+++ nethub/public/js/main.js
@@ -0,0 +1,153 @@
+$(document).ready(function(){
+  addFilter();
+  changeMenuAlternative();
+  onUserChange();
+});
+
+function addFilter(){
+  var selector = $('.wrapper').find('select');
+  selector.change(function() {
+    filtercontent = $(this).siblings('.filtercontent');
+    if(!filtercontent.find("h4").text().includes($(this).val()) && !$(this).val().includes('default') ){
+      filtercontent.append("\
+        <div class='filter'>\
+          <h4>"+$(this).val()+"</h4>\
+        </div>"
+      );
+      loadSearchData();
+      filtercontent.children().on('click', function() {
+        $(this).remove();
+        loadSearchData();
+      })
+    }
+    $(this).val("default");
+  });
+}
+
+function changeMenuAlternative() {
+  $('.menu div').on('click', function() {
+    $(this).siblings().addClass('notselected')
+    $(this).removeClass('notselected')
+    switch ($(this).index()){
+      case 0:
+        $(".watched").css("visibility", "visible")
+        $(".searched").css("visibility", "hidden")
+        break;
+      case 1:
+        $(".watched").css("visibility", "hidden")
+        $(".searched").css("visibility", "visible")
+        break;
+    }
+  })
+}
+
+function onUserChange(){
+  $(".userselect select").change(function() {
+    loadWatchedData()
+    loadSearchData()
+  });
+}
+
+function loadSearchData() {
+  filtervalues = {};
+  filtervalues.languages = getTextFromTags($("#Languages_id").find("h4"))
+  filtervalues.genres = getTextFromTags($("#Genre_id").find("h4"))
+  filtervalues.subtitles = getTextFromTags($("#Subtitles_id").find("h4"))
+  filtervalues.actors = getTextFromTags($("#Actors_id").find("h4"))
+  filtervalues.directors = getTextFromTags($("#Directors_id").find("h4"))
+  filtervalues.movietype = getTextFromTags($("#MovieType_id").find("h4"))
+  filtervalues.rating = getTextFromTags($("#Rating_id").find("h4"))
+
+  console.log(filtervalues)
+  $.ajax({
+    url: "/searched",
+    type: "post",
+    data: { filters: JSON.stringify(filtervalues), user: getSelectedUser() }
+  }).done(function( data ) {
+    $(".searched").empty()
+    appendMovieDiv(data, $(".searched"))
+  })
+}
+
+function getTextFromTags(tags) {
+  var values = [];
+  tags.each(function() {
+    values.push($(this).text())
+  });
+
+  return values
+}
+
+function loadWatchedData(){
+  $.ajax({
+    url: "/watched",
+    type: "post",
+    data: { user: getSelectedUser() }
+    }).done(
+    function( data ){
+      if(data != 'default'){
+        $(".watched").empty()
+        appendMovieDiv(data, $(".watched"));
+        setRatingHover();
+        updateDbRating();
+      }else{
+        $(".watched").empty()
+      }
+    }
+  );
+}
+
+function appendMovieDiv(data, parent) {
+  for (var i = 0; i < data.length; i++) {
+    parent.append("\
+    <div class='movie'>\
+      <h4>"+data[i].moviename+"</h4> \
+      <div class='movierate'>\
+        "+getDivStringFromRating(data[i].rating)+"\
+      </div>\
+    </div>"
+    );
+  }
+}
+
+function setRatingHover(){
+  $(".watched").find(".movierate div").on('mouseover', function() {
+    var currentRating = $(this).parent().children("div.selected").length;
+    var siblingsandself = $(this).parent().children();
+    setSelectedRating(siblingsandself, $(this).index())
+  })
+}
+
+function setSelectedRating(divs, rating) {
+  divs.each(function() {
+    if($(this).index() <= rating)
+      $(this).addClass('selected')
+    else
+      $(this).removeClass('selected')
+  })
+}
+
+function updateDbRating(){
+  $(".watched").find(".movierate div").on('click', function() {
+    var currentRating = $(this).index() + 1
+    var movieName = $(this).parent().siblings("h4").text()
+    var userName = getSelectedUser()
+    $.post("/updaterating", {rating: currentRating, movie: movieName, user: userName})
+  })
+}
+
+function getSelectedUser(){
+  return $(".userselect select option:selected").text()
+}
+
+function getDivStringFromRating(rating){
+  var divstring = ""
+  for (var i = 0; i < 5; i++) {
+    if(i < rating){
+      divstring+="<div class='selected'></div>"
+    }else{
+      divstring+="<div></div>"
+    }
+  }
+  return divstring
+}
---
+++ nethub/routes/index.js
@@ -0,0 +1,216 @@
+var express = require('express');
+var async = require('async');
+const { Pool } = require('pg');
+var router = express.Router();
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'password',
+  port: 5432,
+})
+
+var filtergroups = [
+  'languages',
+  'genres',
+  'actors',
+  'directors',
+  'users',
+  'movies'
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  values = new Object();
+  var i = 0;
+
+  async.forEach(filtergroups, function(filtergroup, callback) {
+    pool.query('SELECT * FROM '+filtergroup, (err, result) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        values[filtergroup] = result.rows
+        i++;
+        tryRender(res, i)
+      }
+    })
+  }, function(err) {
+      if (err) return next(err);
+      // all queries are done here
+  });
+});
+
+router.post('/watched', function(req, res){
+  user = req.body.user
+
+  var watchquery = "SELECT users.name AS username, \
+  movies.name AS moviename, \
+  watched.rating \
+  FROM watched \
+  JOIN movies ON movies.ID = watched.movie_ID \
+  JOIN users ON users.ID = watched.user_ID \
+  WHERE users.name = '"+user+"'";
+
+  if(/^default$/.test(user)){
+    console.log("User cannot be \"Default\"")
+    res.send(req.body.user);
+  }else{
+    pool.query(watchquery, (err, result) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        console.log("Query successful")
+        res.send(result.rows);
+      }
+    })
+  }
+});
+
+router.post("/updaterating", function(req, res){
+  console.log(req.body.movie + ", " + req.body.rating+ ", "+ req.body.user);
+  var updatequery = "UPDATE watched \
+  SET rating = "+req.body.rating+" \
+  WHERE movie_id = \
+    (SELECT ID FROM movies WHERE name = '"+req.body.movie+"') \
+  AND user_id = \
+    (SELECT id FROM users WHERE name = '"+req.body.user+"')";
+
+  pool.query(updatequery, (err, result) => {
+    if (err) {
+      console.log(err.stack)
+    } else {
+      console.log("Query successful")
+      res.send(result.rows);
+    }
+  })
+});
+
+router.post("/searched", function(req, res) {
+  var filtervalues = JSON.parse(req.body.filters)
+  var query = "\
+  SELECT name AS moviename, round(avg(rating)) AS rating FROM movies \
+  JOIN watched ON movies.id = watched.movie_id \
+  WHERE id IN (";
+  var count = 0;
+
+  if(filtervalues.languages.length > 0){
+    query += "(SELECT movie_id \
+      FROM language_in \
+      WHERE language_id IN \
+      (SELECT id FROM languages WHERE language IN "+getArrayAsSQLString(filtervalues.languages)+") \
+      AND type = 'spoken') INTERSECT "
+    count++;
+  }
+
+  if(filtervalues.genres.length > 0){
+    query += "(SELECT movie_id \
+      FROM genre_of \
+      WHERE genre_id IN \
+      (SELECT id FROM genres WHERE name IN "+getArrayAsSQLString(filtervalues.genres)+")) INTERSECT "
+    count++;
+  }
+
+  if(filtervalues.subtitles.length > 0){
+    query += "(SELECT movie_id \
+      FROM language_in \
+      WHERE language_id IN \
+      (SELECT id FROM languages WHERE language IN "+getArrayAsSQLString(filtervalues.subtitles)+") \
+      AND type = 'subtitle') INTERSECT "
+    count++;
+  }
+
+  if(filtervalues.actors.length > 0){
+    query += "(SELECT movie_id \
+      FROM stars_in \
+      WHERE actor_id IN \
+      (SELECT id FROM actors WHERE name IN "+getArrayAsSQLString(filtervalues.actors)+")) INTERSECT "
+    count++;
+  }
+
+  if(filtervalues.directors.length > 0){
+    query += "(SELECT movie_id \
+      FROM directs \
+      WHERE director_id IN \
+      (SELECT id FROM directors WHERE name IN "+getArrayAsSQLString(filtervalues.directors)+")) INTERSECT "
+      count++;
+  }
+
+  if(filtervalues.movietype.length > 0){
+    if(filtervalues.movietype.includes('Series')){
+      query += "(SELECT movie_id FROM movie_in) INTERSECT "
+      count++;
+    }
+
+    if(filtervalues.movietype.includes('Movie')){
+      query += "(SELECT id FROM movies WHERE id NOT IN (SELECT movie_id FROM movie_in)) INTERSECT "
+      count++;
+    }
+  }
+
+  if(filtervalues.rating.length > 0){
+    query += "(SELECT movie_id FROM \
+      (SELECT movie_id, round(avg(rating)) \
+      FROM watched \
+      GROUP BY movie_id) AS foo \
+      WHERE round IN "+getArrayAsSQLString(filtervalues.rating)+") INTERSECT "
+    count++;
+  }
+
+  query = query.replace(/(.+)INTERSECT $/, "$1");
+  query += ")"
+
+  if( count == 0){
+    query = "SELECT name AS moviename, round(avg(rating)) AS rating FROM movies \
+    JOIN watched ON movies.id = watched.movie_id WHERE movies.id NOT IN \
+      (SELECT movie_id FROM watched WHERE user_id = \
+        (SELECT id FROM users WHERE name = '"+req.body.user+"')) "
+  }else{
+    query+= "AND id NOT IN (SELECT movie_id FROM watched WHERE user_id = \
+      (SELECT id FROM users WHERE name = '"+req.body.user+"')) "
+  }
+
+
+
+  query += "GROUP BY movies.name";
+
+  console.log(query)
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.log(err.stack)
+    } else {
+      console.log("Query successful")
+      res.send(result.rows);
+    }
+  })
+});
+
+function getArrayAsSQLString(array){
+  var arrayString = "(";
+  for (var i = 0; i < array.length; i++) {
+    if(i > 0)
+      arrayString+=","
+    arrayString+="\'"+array[i]+"\'"
+  }
+  return arrayString+")"
+}
+
+function tryRender(res, i){
+  if(i == filtergroups.length){
+    renderData(res, values);
+  }
+}
+
+function renderData(res, values) {
+  res.render('index', {
+    users: values['users'],
+    languages: values['languages'],
+    genres: values['genres'],
+    subtitles: values['languages'],
+    actors: values['actors'],
+    directors: values['directors']
+  });
+}
+
+
+module.exports = router;
